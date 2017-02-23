aae6d0fe0fe08151ce948d67164b65774bd9e14c
Levi R <ilevidr@gmail.com>
2017-02-23 15:39:54 -0300

    created menu and home views

--- android-solution/AndroidSolution/app/src/main/java/io/github/pino/androidsolution/MainActivity.java
+++ android-solution/AndroidSolution/app/src/main/java/io/github/pino/androidsolution/MainActivity.java
@@ -1,13 +1,113 @@
 package io.github.pino.androidsolution;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.support.v7.app.ActionBarDrawerToggle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mMenu;
+    private ListView mMenuList;
+    private String[] mMenuListItems;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initActionBar();
+        initMenu();
+
+
     }
+
+    /* Action bar and menu setup */
+
+    private void initActionBar() {
+        getSupportActionBar().setTitle(R.string.home_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    private void initMenu() {
+        mMenuListItems = getResources().getStringArray(R.array.drawer_array);
+        mMenu = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mMenuList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set up menu list
+        mMenuList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mMenuListItems));
+        mMenuList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mMenu,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(R.string.home_title);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(R.string.drawer_title);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mMenu.addDrawerListener(mDrawerToggle);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectMenuItem(position);
+        }
+    }
+
+    private void selectMenuItem(int position) {
+        // TODO: change activities
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Check for drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
 }
